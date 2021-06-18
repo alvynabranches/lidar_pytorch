00fd84bee3deb765207dbd7cbe3a678ac1c9d56c
alvynabranches <alvynabranches@gmail.com>
2021-06-19 00:24:14 +0530

    19062021

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print(f'root {directory=}')
 
 for d in data:
     for dt in d:
-        [print(e) for e in d] if type(dt) is list else print(dt)
+        [print(e) for e in dt] if type(dt) is list else print(dt)
     print()
     print()
     print()
